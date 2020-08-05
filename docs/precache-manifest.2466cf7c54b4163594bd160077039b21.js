123a5c6ad5105ca90ec9b7b9229664aa1bdef3cc
Jhon Obando <hola@jhonobando.cl>
2020-08-05 00:44:31 -0400

    add docs

---
+++ docs/precache-manifest.2466cf7c54b4163594bd160077039b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526de59d04746858196f3804398d9c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293abeb85469f51bc5e2",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "97a9b182951b58017190",
+    "url": "/static/css/main.e6292434.chunk.css"
+  },
+  {
+    "revision": "293abeb85469f51bc5e2",
+    "url": "/static/js/2.8c695d5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c695d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a9b182951b58017190",
+    "url": "/static/js/main.673006b1.chunk.js"
+  },
+  {
+    "revision": "fbc7abd2ff89b66febf9",
+    "url": "/static/js/runtime-main.4d9809d9.js"
+  }
+]);
